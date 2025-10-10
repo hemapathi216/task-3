b0c1c5fb6f8f59d93b8c67df0392de8c4ed1c7a6
hemapathi216 <hemapathidhanam@gmail.com>
2025-10-10 16:57:51 +0530

    day5

---
+++ day5.py
@@ -0,0 +1,29 @@
+#task1
+for i in range(10,0,-1):
+    print(i)
+#task2
+x=0
+while x<5:
+    x+=1
+    print("hema")
+#task3
+x=0
+while x<10:
+    x+=1
+    print("hema")
+#task4
+x=0
+while x<10:
+    x+=1
+    print("hema")
+    if(x==5):
+        break;
+#task5
+f=1
+num=(int(input("enter the number:")))
+for num in range(1,num+1):
+    f=f*num
+print("factorial is",f)
+     
+     
+    
